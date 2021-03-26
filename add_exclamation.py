3211531765f1e24f3195f6cc742a6d194fa15072
Philip Worswick <72656207+Phill-inC0DE@users.noreply.github.com>
2021-03-26 18:23:09 +0000

    update

--- add_exclamation.py
+++ add_exclamation.py
@@ -1,3 +1,5 @@
+# add an exclamation mark in the white space, from the end of the word until it reaches 20 characters.
+
 def add_exclamation(word):
   for x in range(len(word),20):
     word += "!"
